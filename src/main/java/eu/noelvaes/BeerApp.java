7f21bc976a4395069939a1924da54c3470f75e14
Marc J <john.doe.gmail.com>
2020-10-20 13:37:14 +0200

    JdbcTemplate werkt niet

--- src/main/java/eu/noelvaes/BeerApp.java
+++ src/main/java/eu/noelvaes/BeerApp.java
@@ -1,11 +1,17 @@
 package eu.noelvaes;
 
+import eu.noelvaes.repositories.BeerDao;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class BeerApp {
     public static void main(String[] args) {
-        SpringApplication.run(BeerApp.class, args);
+        ConfigurableApplicationContext ctx =
+                SpringApplication.run(BeerApp.class, args);
+        BeerDao dao = ctx.getBean("beerDao", BeerDao.class);
+        System.out.println(dao.getBeersById(20));
+        //dao.setStock(10, 200);
     }
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,12 +3,8 @@ open module eu.noelvaes.spring{
     requires spring.context;
     requires spring.beans;
     requires spring.boot;
-    requires spring.jdbc;
     requires spring.boot.autoconfigure;
     requires spring.boot.starter.jdbc;
-    requires spring.boot.starter.aop;
+    requires spring.jdbc;
     requires java.sql;
-    requires java.logging;
-    requires java.annotation;
-    requires org.aspectj.weaver;
 }
